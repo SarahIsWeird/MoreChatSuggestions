906c6549ae9651a2eeb86cbd3b6c618d4283672b
SarahIsWeird <morningcrafter0603@gmail.com>
2022-10-16 01:10:42 +0200

    Add more javadocs

--- src/main/java/com/sarahisweird/morechatsuggestions/SuggestionCondition.java
+++ src/main/java/com/sarahisweird/morechatsuggestions/SuggestionCondition.java
@@ -7,7 +7,16 @@ package com.sarahisweird.morechatsuggestions;
  */
 @FunctionalInterface
 public interface SuggestionCondition {
+    /**
+     * See {@link SuggestionCondition} for more information.
+     *
+     * @param currentWord The current word
+     * @return {@code true} if the suggestion class should be added to the autocomplete suggestions.
+     */
     boolean shouldAddSuggestions(String currentWord);
 
+    /**
+     * This should be used if the suggestions should be always added to the collection.
+     */
     SuggestionCondition ALWAYS = currentWord -> true;
 }
